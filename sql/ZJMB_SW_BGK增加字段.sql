57ac7ebf02c95d1875aa8f87485046b7932f2819
xiaomuMK <xixiaomu163@163.com>
2019-04-11 14:43:20 +0800

    sql update

--- sql/ZJMB_SW_BGK增加字段.sql
+++ sql/ZJMB_SW_BGK增加字段.sql
@@ -1,2 +1,2 @@
- alter table zjmb_sw_bgk add (VC_UP_SP_FLAG  number(1) default 0);
+alter table zjmb_sw_bgk add (VC_UP_SP_FLAG  number(1) default 0);
 COMMENT ON COLUMN zjmb_sw_bgk.VC_UP_SP_FLAG IS '上传台州共享平台标识， 0 为未上传 1 为已上传';
